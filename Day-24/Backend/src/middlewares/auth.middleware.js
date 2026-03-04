0a9e207ea00b417f1675279caf73e09965bf5fe0
Darshanwalher <darshanwalher21@gmail.com>
2026-03-04 23:33:51 +0530

    auth

---
+++ Day-24/Backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const blackList = require("../models/blacklist.model");
+
+
+
+async function authUser(req,res,next){
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message:"token not provided."
+        })
+    }
+
+    const isBlackListed = await blackList.findOne({
+        token
+    })
+
+    if(isBlackListed){
+        return res.status(401).json({
+            message:"token is blacklisted."
+        })
+    }
+
+
+    let decode;
+
+    try{
+        decode = jwt.verify(token,process.env.JWT_SECRET_KEY)
+        req.user = decode
+        next();
+    }
+    catch(err){
+        return res.status(401).json({
+            message:"invalid token."
+        })
+    
+    }
+
+}
+
+module.exports = authUser;
---
+++ Day-24/Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FaceExpression from './features/components/FaceExpression'
+
+const App = () => {
+  return (
+    <div>
+      <FaceExpression />
+    </div>
+  )
+}
+
+export default App
