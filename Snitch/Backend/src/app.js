ffe576028f10a0c6ce3e9efb6613c4dc422a0060
Darshanwalher <darshanwalher21@gmail.com>
2026-06-17 00:41:17 +0530

    Refactor code structure for improved readability and maintainability

--- Snitch/Backend/src/app.js
+++ Snitch/Backend/src/app.js
@@ -18,7 +18,9 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
 }));
+app.use(express.static("./public"));
 
 app.use(passport.initialize());
 passport.use(new GoogleStrategy({
@@ -36,5 +38,11 @@ app.get("/",(req,res)=>{
 app.use("/api/auth", authRouter);
 app.use("/api/products", ProductRouter);
 app.use("/api/cart", cartRouter);
+
+// Catch-all route to serve the frontend React/Vue app for any unknown routes (Fixes "Cannot GET /login")
+app.get(/.*/, (req, res) => {
+    res.sendFile(path.resolve("public", "index.html"));
+});
+
 
 export default app;
