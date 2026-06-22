959dbbd45677bd1c52e3ab68e39d4e07c73e68b4
Darshanwalher <darshanwalher21@gmail.com>
2026-06-22 15:55:05 +0530

    chore: untrack and ignore nested Snitch repository

--- Snitch/Backend/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./src/app.js";
-import connectDB from "./src/config/database.js";
-import { config } from "./src/config/config.js";
-
-connectDB();
-
-app.listen(config.PORT, () => {
-    console.log(`Server is running on port ${config.PORT}`);
-});
